44dfb45cabd04263b73ed9a12c9c56f37b7d3cce
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-08 15:30:57 +0530

    extension.json changed

--- offlineextension.php
+++ offlineextension.php
@@ -1,15 +1,16 @@
 <?php
-
-if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'offlineextension' );
-	// Keep i18n globals so mergeMessageFileList.php doesn't break
-	$wgMessagesDirs['offlineextension'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['offlineextensionAlias'] = __DIR__ . '/offlineextension.i18n.alias.php';
-	wfWarn(
-		'Deprecated PHP entry point used for offlineextension extension. Please use wfLoadExtension ' .
-		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);
-	return true;
-} else {
-	die( 'This version of the offlineextension extension requires MediaWiki 1.25+' );
+/*Entry point */
+if (!defined('MEDIAWIKI')){
+    die();
 }
+if(function_exists('wfLoadExtension')) {
+    wfLoadExtension('offlineextension');
+
+    wfWarn( "Deprecated entry point to offlineextension. Please use wfLoadExtension('offlineextension').");
+
+}
+else
+{
+    die("MediaWiki version 1.25+ is required to use the offlineextension extension");
+}
+?>
