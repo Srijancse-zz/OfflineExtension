daab658526ea956d1c7b557bec3052814ef89526
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-09 16:26:27 +0530

    Added the offline js and css files in extension.json

--- specials/Specialofflineextension.php
+++ specials/Specialofflineextension.php
@@ -1,25 +1,18 @@
 <?php
-/**
- * HelloWorld SpecialPage for offlineextension extension
- *
- * @file
- * @ingroup Extensions
- */
+
 
 class SpecialHelloWorld extends SpecialPage {
 	public function __construct() {
 		parent::__construct( 'HelloWorld' );
 	}
 
-	/**
-	 * Show the page to the user
-	 *
-	 * @param string $sub The subpage string argument (if any).
-	 *  [[Special:HelloWorld/subpage]].
-	 */
+
 	public function execute( $sub ) {
 		$out = $this->getOutput();
-
+		$out->addModules( 'offline' );
+47	$out->addModuleStyles('offline-theme-default');
+		$out->addModuleStyles('offline-language-english');
+		$out->addModuleStyles('offline-language-english-indicator');
 		$out->setPageTitle( $this->msg( 'offlineextension-helloworld' ) );
 
 		$out->addHelpLink( 'How to become a MediaWiki hacker' );
