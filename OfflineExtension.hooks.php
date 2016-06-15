a8a751a68dcf2802cc453e29c613ffb5bd0547c2
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-15 14:00:54 +0530

    Linked CSS files in hooks

--- OfflineExtension.hooks.php
+++ OfflineExtension.hooks.php
@@ -1,10 +1,14 @@
 <?php
+
 class OfflineExtensionHooks
 {
+
   public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 
-    $out->addModules( array( 'ext.OfflineExtension' ) )
-    
+    $out->addModules( array( 'ext.OfflineExtension' ) );
+    $out->addModules( array('ext.offlineextension.foo-indicator','ext.offlineextension.foo-english','ext.offlineextension.foo-theme'));
+
   }
+
+
 }
-Raw
--- OfflineExtension.php
+++ OfflineExtension.php
@@ -6,6 +6,7 @@ if (function_exists('wfLoadExtension')) {
     wfWarn(
         'Deprecated PHP entry point used for OfflineExtension extension. Please use wfLoadExtension instead, '.'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
     );
+
     return;
 } else {
     die('This version of the OfflineExtension extension requires MediaWiki 1.25+');
