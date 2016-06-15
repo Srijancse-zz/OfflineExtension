9be808a8854614138a6cee5e2eb5e824f666a74f
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-15 13:36:36 +0530

    wgMessagesDirs added and removed wgResourceModules

--- OfflineExtension.php
+++ OfflineExtension.php
@@ -1,30 +1,12 @@
 <?php
-/*Entry point */
-if (!defined('MEDIAWIKI')){
-    die();
-}
-if(function_exists('wfLoadExtension')) {
+if (function_exists('wfLoadExtension')) {
     wfLoadExtension('OfflineExtension');
-
-  $wgResourceModules['ext.offlineextension.foo'] = array(
-	'position' => 'top',
-	'scripts' => 'modules/ext.offlineextension.foo.js',
-	'styles' => array( 'modules/ext.offlineextension.foo-indicator.css','modules/ext.offlineextension.foo-english.css','modules/ext.offlineextension.foo-theme.css' ),
-	'messages' => array(
-		'OfflineExtension-foo-label',
-	),
-	'dependencies' => array(
-		'jquery.cookie',
-		'jquery.tabIndex',
-	),
-
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'OfflineExtension',
-);
-
+    // Keep i18n globals so mergeMessageFileList.php doesn't break
+    $wgMessagesDirs['OfflineExtension'] = __DIR__.'/i18n';
+    wfWarn(
+        'Deprecated PHP entry point used for OfflineExtension extension. Please use wfLoadExtension instead, '.'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+    );
+    return;
+} else {
+    die('This version of the OfflineExtension extension requires MediaWiki 1.25+');
 }
-else
-{
-    die("MediaWiki version 1.25+ is required to use the OfflineExtension extension");
-}
-?>
