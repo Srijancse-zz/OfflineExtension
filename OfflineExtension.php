4a285ab0bd35f2f6c841c949c21a09085708b4e6
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-15 11:36:26 +0530

    Added ResourceModules function

--- OfflineExtension.php
+++ OfflineExtension.php
@@ -6,7 +6,21 @@ if (!defined('MEDIAWIKI')){
 if(function_exists('wfLoadExtension')) {
     wfLoadExtension('OfflineExtension');
 
-    wfWarn( "Deprecated entry point to OfflineExtension. Please use wfLoadExtension('OfflineExtension').");
+  $wgResourceModules['ext.offlineextension.foo'] = array(
+	'position' => 'top',
+	'scripts' => 'modules/ext.offlineextension.foo.js',
+	'styles' => array( 'modules/ext.offlineextension.foo-indicator.css','modules/ext.offlineextension.foo-english.css','modules/ext.offlineextension.foo-theme.css' ),
+	'messages' => array(
+		'OfflineExtension-foo-label',
+	),
+	'dependencies' => array(
+		'jquery.cookie',
+		'jquery.tabIndex',
+	),
+
+	'localBasePath' => __DIR__,
+	'remoteExtPath' => 'OfflineExtension',
+);
 
 }
 else
