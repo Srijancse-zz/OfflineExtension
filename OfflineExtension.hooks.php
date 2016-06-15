32ea6e73b5cd84818f212caa729a218d3b0c91a3
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-15 16:29:51 +0530

    Spelling mistake

--- OfflineExtension.hooks.php
+++ OfflineExtension.hooks.php
@@ -5,8 +5,8 @@ class OfflineExtensionHooks
 
   public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 
-    $out->addModules( array( 'ext.OfflineExtension' ) );
-    $out->addModuleStyles( array('ext.offlineextension.foo-indicator','ext.offlineextension.foo-english','ext.offlineextension.foo-theme'));
+    $out->addModules( array( 'ext.offlineextension' ) );
+    $out->addModuleStyles( array( 'ext.offlineextension.foo-indicator','ext.offlineextension.foo-english','ext.offlineextension.foo-theme'));
 
   }
 
