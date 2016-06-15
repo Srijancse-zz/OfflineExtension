b1db1c5cb98639c1239a0e6b4c0775e56782a473
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-15 16:31:18 +0530

    Typo

--- OfflineExtension.hooks.php
+++ OfflineExtension.hooks.php
@@ -5,7 +5,7 @@ class OfflineExtensionHooks
 
   public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 
-    $out->addModules( array( 'ext.offlineextension' ) );
+    $out->addModules( array( 'ext.offlineextension.foo' ) );
     $out->addModuleStyles( array( 'ext.offlineextension.foo-indicator','ext.offlineextension.foo-english','ext.offlineextension.foo-theme'));
 
   }
