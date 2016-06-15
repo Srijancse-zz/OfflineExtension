8105ca77fdc7502fe892cb6a0dba3358cf2b1dd5
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-15 14:16:43 +0530

    Corrected addModules to addModuleStyles

--- OfflineExtension.hooks.php
+++ OfflineExtension.hooks.php
@@ -6,7 +6,7 @@ class OfflineExtensionHooks
   public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
 
     $out->addModules( array( 'ext.OfflineExtension' ) );
-    $out->addModules( array('ext.offlineextension.foo-indicator','ext.offlineextension.foo-english','ext.offlineextension.foo-theme'));
+    $out->addModuleStyles( array('ext.offlineextension.foo-indicator','ext.offlineextension.foo-english','ext.offlineextension.foo-theme'));
 
   }
 
