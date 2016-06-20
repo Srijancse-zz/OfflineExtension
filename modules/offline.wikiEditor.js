89076b184f334a9387e7ffaac7c4e8234fd2d5eb
Srijancse <srijanagarwal.cse@gmail.com>
2016-06-20 22:12:40 +0530

    JS alerts the user while editing if goes offline

---
+++ modules/offline.wikiEditor.js
@@ -0,0 +1,34 @@
+//function to notify the user if he's editing
+
+$('#wpTextbox1').bind('input propertychange', function() {
+
+//An ajax call is done to server-side page. If the browser is offline, then the connection will fail and,
+//thus, the oneerror event will be called. Otherwise, the onload event is called.
+
+
+    function isOnline(no, yes) {
+        var xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHttp');
+        xhr.onload = function() {
+            if (yes instanceof Function) {
+                yes();
+            }
+        }
+        xhr.onerror = function() {
+            if (no instanceof Function) {
+                no();
+            }
+        }
+        xhr.open("HEAD", "WikiEditor.php", true); //We can change the URL to point to any file that exists on server.
+        xhr.send();
+    }
+
+    isOnline(
+        function() {
+            alert("Offline and User is editing");
+        },
+        function() {
+            alert("Online");
+        }
+    );
+
+});
